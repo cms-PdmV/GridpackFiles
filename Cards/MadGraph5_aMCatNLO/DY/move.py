230d038b688aac1188a8ec1d93465bf541376215
Sihyun Jeon <shjeon@cern.ch>
2022-12-06 06:01:12 +0900

    update DY

--- Cards/MadGraph5_aMCatNLO/DY/move.py
+++ Cards/MadGraph5_aMCatNLO/DY/move.py
@@ -12,9 +12,10 @@ for sample in samples:
     os.chdir(sample)
 
     files = os.listdir("./")
-    for file in files:
-        os.system(f"mv {file} {file.replace('DYto2L', 'DYto2L-4Jets')}")
-    os.system(f"sed -i '' 's|{file}|{file.replace('DYto2L', 'DYto2L-4Jets')}|g' *.dat")
+#    for file in files:
+#        os.system(f"mv {file} {file.replace('DYto2L', 'DYto2L-4Jets')}")
 
     os.chdir(cwd)
-    os.system(f"mv {sample} {sample.replace('DYto2L', 'DYto2L-4Jets')}")
+    print(f"sed -i '' 's|{sample.replace('-4Jets', '')}|{sample.replace('-4Jets', '').replace('DYto2L', 'DYto2L-4Jets')}|g' {sample}/{sample}_proc_card.dat")
+
+#    os.system(f"mv {sample} {sample.replace('DYto2L', 'DYto2L-4Jets')}")
